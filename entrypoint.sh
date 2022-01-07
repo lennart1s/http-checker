778305979284bcd64d8d59d46956a3eaa101bf2b
lennart1s <sandbothe.lennart@gmail.com>
2022-01-07 17:46:00 +0100

    awdawd

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
-set -e
+#set -e
 
 echo "HTTP-checker starting..."
 
 RESP=$(/app/http-checker "$INPUT_URLS" "$INPUT_CODES")
+#RESP="'https://google.com': 200, 'https://sandbothe.dev': 503<-fail"
 
 echo "${RESP}"
 
 echo "::set-output name=responses::${RESP}"
 
 if [[ $RESP == *"<-fail"* ]]; then
+  echo "Fail!"
   exit 1
 fi
+
+echo "Done"
