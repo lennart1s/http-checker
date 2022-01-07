2c9d18655c60414fe33dc18b4945188a2173fbc1
lennart1s <sandbothe.lennart@gmail.com>
2022-01-07 17:22:19 +0100

    .

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,6 @@ echo "HTTP-checker starting..."
 
 #INPUT_URLS="[5, 6, 10]"
 
-RESP=$(/app/http-checker "$INPUT_URLS" "$INPUT_CODES")
-
-echo "${RESP}"
+RESP=$(/app/http-checker "$INPUT_URLS" "$INPUT_CODES" | tail -1)
 
 echo "::set-output name=responses::${RESP}"
